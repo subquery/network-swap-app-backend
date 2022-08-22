5f588f993172d28a68f3d7e6706a6bd4bbb9ab4b
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2022-08-23 11:10:36 +1200

    add chaintypes, update deps, update project.yaml

---
+++ chaintypes.js
@@ -0,0 +1,6 @@
+Object.defineProperty(exports, '__esModule', { value: true });
+const moonbeam_types_bundle_1 = require('moonbeam-types-bundle');
+
+exports.default = {
+  typesBundle: moonbeam_types_bundle_1.typesBundleDeprecated,
+};
